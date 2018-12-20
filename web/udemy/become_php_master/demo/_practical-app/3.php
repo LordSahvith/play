ff67fd0d08173360509697e04ee107d9c20b9055
Caleb <bulletproofchild@gmail.com>
2018-12-19 21:35:20 -0700

    Lecture 21 - Pratical App Part 3

--- web/udemy/become_php_master/demo/_practical-app/3.php
+++ web/udemy/become_php_master/demo/_practical-app/3.php
@@ -1,18 +1,19 @@
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
-	<aside class="col-xs-4">
+  <aside class="col-xs-4">
 
-	<?php Navigation();?>
-			
-	</aside><!--SIDEBAR-->
+    <?php Navigation();?>
+
+  </aside>
+  <!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
+  <article class="main-content col-xs-8">
 
-<?php  
+    <?php  
 
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
@@ -24,6 +25,42 @@
 	Step 3 : Make a switch Statement that test againts one condition with 5 cases
 
  */
+  
+  $sith = true;
+  $jedi = 2;
+  
+  if ($sith) {
+    echo "sith are better.";
+  } else if ($jedi < 3) {
+    echo "jedi are outnumbered.";
+  } else {
+    echo "sith win with php.";
+  }
+  
+  echo "<br>";
+  
+  
+  for ($bounty = 0; $bounty <= 10; $bounty++) {
+    echo $bounty . " bounty partially paid.<br>";
+  }  
+  
+  echo "bounty paid.<br>";
+  
+    $batman = "batman";
+  
+  switch($batman) {
+    case "batman":
+      echo "batman duh.";
+      break;
+    case "catwoman":
+        echo "catwoman.";
+      break;
+    case "superaman":
+      echo "who?";
+      break;
+    default:
+      echo "5 cases.";
+  }
 
 	
 ?>
@@ -33,6 +70,7 @@
 
 
 
-</article><!--MAIN CONTENT-->
-	
-<?php include "includes/footer.php" ?>
+  </article>
+  <!--MAIN CONTENT-->
+
+  <?php include "includes/footer.php" ?>
