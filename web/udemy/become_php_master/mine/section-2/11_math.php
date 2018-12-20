0ba617a530d73786e004a57d91c789d1886dacbc
Caleb <bulletproofchild@gmail.com>
2018-12-19 19:50:17 -0700

    Lecture 11 - Math

---
+++ web/udemy/become_php_master/mine/section-2/11_math.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Math</title>
+</head>
+
+<body>
+
+  <?php
+  
+  echo 45 + 21;
+  echo "<br>";
+  echo 34 * 2;
+  echo "<br>";
+  echo 90 / 8;
+  echo "<br>";
+  echo 56 % 2;
+  
+  $number1 = 340;
+  $number2 = 1234;
+  
+  echo "<br>";
+  echo $number1 * $number2 / 2.3;
+  
+  ?>
+
+</body>
+
+</html>
