b2b9adad8c9a1e621a92b44bcfba2a0226f2dfd1
Caleb <bulletproofchild@gmail.com>
2018-12-19 19:41:00 -0700

    Lecture 10 - Variables

---
+++ web/udemy/become_php_master/mine/section-2/10_variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Variables</title>
+</head>
+
+<body>
+
+  <?php
+  
+  // variables created by '$' 
+  // followed by name
+  $name = "Sahvith";
+  $number = 26;
+  
+  echo $name;
+  
+  // you can string together variables
+  // called concatenation
+  echo "<br>" . $name . " " . $number;
+  
+  ?>
+
+</body>
+
+</html>
