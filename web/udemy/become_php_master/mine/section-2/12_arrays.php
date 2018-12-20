65e2bc081ef2789c24ccdbcd96cbdae8e7b9eb0c
Caleb <bulletproofchild@gmail.com>
2018-12-19 19:57:26 -0700

    Lecture 12 - Arrays

---
+++ web/udemy/become_php_master/mine/section-2/12_arrays.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Arrays</title>
+</head>
+
+<body>
+
+  <?php
+  
+  // can use array()
+  $numberList = [1, 2, 3, 4, 5];
+  
+  $list = array("strings", 34, "numbers", "34", "<h2>PHP is awesome!</h2>");
+  
+  // echo $numberList[0] + $numberList[4];
+  
+  print_r($numberList);
+  
+  echo $list[4];
+  
+  ?>
+
+</body>
+
+</html>
