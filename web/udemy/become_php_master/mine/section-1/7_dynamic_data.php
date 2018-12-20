84b9f52dc7b31d29f93d8bed76af939266b5977d
Caleb <bulletproofchild@gmail.com>
2018-12-19 19:27:37 -0700

    Lecture 7 - Dynamic Data

---
+++ web/udemy/become_php_master/mine/section-1/7_dynamic_data.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Dynamic Text</title>
+</head>
+
+<body>
+
+  <?php
+    $name = "Lord Sahvith";
+  ?>
+
+  <h1>Hello
+    <?php echo $name; ?>.</h1>
+
+</body>
+
+</html>
