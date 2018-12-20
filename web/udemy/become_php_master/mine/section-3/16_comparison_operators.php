f3232ca460c7ef769155ec7e5f5150c0c625f059
Caleb <bulletproofchild@gmail.com>
2018-12-19 20:56:18 -0700

    Lecture 16 - Comparison Operators

---
+++ web/udemy/become_php_master/mine/section-3/16_comparison_operators.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Comparison Operators</title>
+</head>
+
+<body>
+
+  <?php
+  
+  $Goku = 1000;
+  $Superman = 10000;
+  $Batman = 500;
+  $Spiderman = 800;
+  
+  if ($Goku > $Spiderman) {
+    echo "Goku wins.";
+  } else {
+    echo "Spiderman wins.";
+  }
+  
+  echo "<br>";
+  
+  if ($Batman > $Superman) {
+    echo "Batman wins.";
+  } else {
+    echo "Batman wins. Batman always wins.";
+  }
+  
+  ?>
+
+
+</body>
+
+</html>
