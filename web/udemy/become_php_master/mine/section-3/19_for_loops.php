efdc381482a6deecff9268d7ec906f784ff0dec3
Caleb <bulletproofchild@gmail.com>
2018-12-19 21:21:39 -0700

    Lecture 19 - For Loops

---
+++ web/udemy/become_php_master/mine/section-3/19_for_loops.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>For Loop</title>
+</head>
+
+<body>
+
+  <?php
+  
+  
+  
+  for ($jedi = 4; $jedi >= 0; $jedi--) {
+    echo $jedi . " jedi left<br>";
+  }
+  
+  ?>
+
+</body>
+
+</html>
