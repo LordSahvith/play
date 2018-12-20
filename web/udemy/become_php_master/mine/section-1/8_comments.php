a14b415ceec1e5cdbd8c42a191aa869a7b1f88c9
Caleb <bulletproofchild@gmail.com>
2018-12-19 19:30:35 -0700

    Lecture 8 - Comments

---
+++ web/udemy/become_php_master/mine/section-1/8_comments.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Comments</title>
+</head>
+
+<body>
+
+  <?php
+    // prints to browser screen
+    echo "Hello Lord Sahvith.";
+  
+    /* 
+      hey this allows for more in depth
+      description if needed without 
+      causing the length to get to great
+      for one line.
+    */
+  ?>
+
+</body>
+
+</html>
