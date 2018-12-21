2e9fb39d202136e2af890eae85b0d0f66552ad8d
Caleb <bulletproofchild@gmail.com>
2018-12-20 18:33:28 -0700

    Lecture 27 - Constants

---
+++ web/udemy/become_php_master/mine/section-4/26_constants.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Constants</title>
+</head>
+
+<body>
+
+  <?php
+  
+  
+  $number = 10;
+  echo $number . "<br>";
+  
+  define("NAME", "Sahvith");
+  
+  echo NAME;
+  
+  // constants can't be changed or it will cause error
+  
+//  NAME = "Bob";
+//         
+//  echo NAME;
+  
+  
+  ?>
+
+</body>
+
+</html>
