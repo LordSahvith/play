654ae889a99e3ffb6712a0e83f5f04871f299816
Caleb <bulletproofchild@gmail.com>
2018-12-20 18:43:32 -0700

    Lecture 28 - Math Functions

---
+++ web/udemy/become_php_master/mine/section-5/28_math_functions.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Math Functions</title>
+</head>
+
+<body>
+
+  <?php
+  
+  echo pow(6, 9);
+  
+  echo "<br>";
+  
+  echo rand(1, 500);
+  
+  echo "<br>";
+  
+  echo sqrt(100);
+  
+  echo "<br>";
+  
+  echo ceil(100.3); // round up
+  
+  echo "<br>";
+  
+  echo floor(100.7); // round down
+  
+  echo "<br>";
+  
+  echo round(100.7); // round
+  
+  echo "<br>";
+  
+  echo round(100.3); // round
+  
+  ?>
+
+
+</body>
+
+</html>
