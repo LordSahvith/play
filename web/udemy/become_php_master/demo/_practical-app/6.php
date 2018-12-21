2cd60eba0366e124f4bc19460169e9e072f611f7
Caleb <bulletproofchild@gmail.com>
2018-12-20 20:06:30 -0700

    Lecture 36 - Practical App part 6

--- web/udemy/become_php_master/demo/_practical-app/6.php
+++ web/udemy/become_php_master/demo/_practical-app/6.php
@@ -1,29 +1,55 @@
-
 <?php include "functions.php" ?>
 <?php include "includes/header.php" ?>
 
-	<section class="content">
+<section class="content">
 
-		<aside class="col-xs-4">
-		
-		<?php Navigation();?>
-			
-		</aside><!--SIDEBAR-->
+  <aside class="col-xs-4">
+
+    <?php Navigation();?>
+
+  </aside>
+  <!--SIDEBAR-->
 
 
-<article class="main-content col-xs-8">
- 
+  <article class="main-content col-xs-8">
 
-	<?php  
+
+    <?php  
 
 /*  Step1: Make a form that submits one value to POST super global
 
 
  */
 
+
+if (isset($_POST["submit"])) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  
+  if (strlen($username) < 5) {
+    echo '<p>username not long enough.</p>';
+  } else {
+    echo '<p>Welcome</p>';
+  }
+}
+
+  
+  
+
 	
 ?>
 
+    <form action="6.php" method="post">
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php" ?>
+      <input type="text" name="username" placeholder="enter name">
+      <input type="password" name="password" placeholder="password">
+      <input type="submit" name="submit">
+
+    </form>
+
+
+
+
+  </article>
+  <!--MAIN CONTENT-->
+  <?php include "includes/footer.php" ?>
